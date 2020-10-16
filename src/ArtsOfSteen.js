8745f82285eb367d304551c2140d2c2c14a80121
Rodrigo Poma <rodpoma@gmail.com>
2020-10-16 11:18:36 -0600

    first commit

--- src/ArtsOfSteen.js
+++ src/ArtsOfSteen.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+function ArtsOfSteen() {
   return (
     <div className="App">
       <header className="App-header">
@@ -23,4 +23,4 @@ function App() {
   );
 }
 
-export default App;
+export default ArtsOfSteen;
