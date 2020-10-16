e192c2bc5f2694b4579afbadb3cb4eb2c7de2711
Rodrigo Poma <rodpoma@gmail.com>
2020-10-16 11:24:36 -0600

    components Outlined

--- src/ArtsOfSteen.js
+++ src/ArtsOfSteen.js
@@ -2,7 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function ArtsOfSteen() {
+const ArtsOfSteen = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +21,6 @@ function ArtsOfSteen() {
       </header>
     </div>
   );
-}
+};
 
 export default ArtsOfSteen;
